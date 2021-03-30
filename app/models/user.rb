1f4cf13c7cb82935b31cb7477c300cffd4c7f1bf
russell-macalintal <russell.macalintal@gmail.com>
2021-03-30 11:14:55 -0400

    Revised controller methods.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,16 @@ class User < ApplicationRecord
 
     has_many :plays
     has_many :games, through: :plays
-    
+
     has_many :console_users
     has_many :consoles, through: :console_users
 
     has_secure_password
+
+    def new
+    end
+
+    def create
+    end
+    
 end
