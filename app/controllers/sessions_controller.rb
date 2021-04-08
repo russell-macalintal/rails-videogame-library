92f18273ab09c1888eb68195ecb918a3816fb0c4
russell-macalintal <russell-macalintal@gmail.com>
2021-04-08 02:54:11 -0400

    Implement session controller actions.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(name: params[:name])
+    if @user.nil?
+      redirect_to '/login'
+    else
+      if @user.autheticate(params[:password])
+        session[:user_id] = @user.id
+      else
+        redirect_to '/login'
+      end
+    end
   end
 
   def destroy
+    session.destroy
+    redirect_to root_path
   end
 end
