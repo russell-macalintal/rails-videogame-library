2496801e502ffa8a27158256b5d4eff98b01f863
russell-macalintal <russell-macalintal@gmail.com>
2021-04-20 00:34:57 -0400

    Added consoles#new and consoles#edit actions and views

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -8,7 +8,47 @@ class ConsolesController < ApplicationController
         end
     end
 
+    def new
+        @console = Console.new
+    end
+
+    def create
+        if Console.where(name: params[:console][:name]).exists?
+            flash[:notice] = "Console Already Exists"
+        else
+            @console = Console.create(console_params)
+            flash[:notice] = "Console Successfully Created"
+        end
+
+        redirect_to consoles_path
+    end
+
     def show
         @console = Console.find(params[:id])
+    end
+
+    def edit
+        @console = Console.find(params[:id])
+    end
+
+    def update
+        @console = Console.find(params[:id])
+        if @console.update(console_params)
+            flash[:notice] = "Console Details Edited Successfully"
+        else
+            flash[:notice] = "Oops! Something went wrong. Try again."
+        end
+        redirect_to console_path(@console)
+    end
+
+    def destroy
+        Console.find(params[:id]).destroy
+        flash[:notice] = "Console Deleted From Library"
+        redirect_to consoles_path
+    end
+
+    
+    def console_params
+        params.require(:console).permit(:name, :manufacturer)
     end
 end
