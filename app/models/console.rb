d8ddfd58e4af0a6eb48252b08995bd3885c1e3f2
russell-macalintal <russell-macalintal@gmail.com>
2021-04-24 15:38:32 -0400

    Refactored Console.most_populer method to show all consoles tied for most number of users

--- app/models/console.rb
+++ app/models/console.rb
@@ -5,11 +5,20 @@ class Console < ApplicationRecord
     validates :name, uniqueness: true
 
     def self.most_popular
-        # console_id = self.joins(:users)
-        #     .select("users.*, count(users.id) as ucount")
-        #     .group("users.id")
-        #     .order("ucount DESC")
-        #     .first.id
+        # This code only allows one console object to be shown, but multiple consoles can have the most number of users
+        # console_id = ConsoleUser.select(:console_id).group(:console_id).order("count(console_id) desc").first.console_id
         # self.find(console_id)
+
+        console_id = ConsoleUser.select(:console_id).group(:console_id).order("count(console_id) desc").first.console_id
+        user_count = self.find(console_id).users.count
+
+        cons_arr = []
+        self.all.each do |cons|
+            if cons.users.count == user_count
+                cons_arr << cons
+            end
+        end
+
+        cons_arr
     end
 end
